5d50f7536156e6ee618c01d80877aa4481442ae0
Jay Bazuzi <jay@bazuzi.com>
2013-09-15 12:20:38 -0700

    REFACTOR: Move `MockLock` to its own file

--- LocksExperiment1/LocksExperiment1/CounterTests.cs
+++ LocksExperiment1/LocksExperiment1/CounterTests.cs
@@ -9,40 +9,6 @@ namespace LocksExperiment1
 {
     public class CounterTests
     {
-        class MockLock<T> : ILock
-        {
-            public event EventHandler<T> OnAcquire = delegate { };
-            bool acquired = false;
-            IDisposable ILock.Acquire()
-            {
-                this.OnAcquire(this, this.counter);
-                this.acquired = true;
-                return this;
-            }
-
-            bool ILock.IsLocked
-            {
-                get
-                {
-                    return this.acquired && !this.disposed;
-                }
-            }
-
-            public event EventHandler<T> OnDispose = delegate { };
-            bool disposed = false;
-            void IDisposable.Dispose()
-            {
-                this.OnDispose(this, this.counter);
-                this.disposed = true;
-            }
-
-            T counter;
-            internal void SetCounter(T counter)
-            {
-                this.counter = counter;
-            }
-        }
-
         public class MockLockTests : LockTests
         {
             protected override ILock Create()
@@ -50,7 +16,6 @@ namespace LocksExperiment1
                 return new MockLock<Counter>();
             }
         }
-
 
         Counter Create()
         {
---
+++ LocksExperiment1/LocksExperiment1/MockLock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocksExperiment1
+{
+    class MockLock<T> : ILock
+    {
+        public event EventHandler<T> OnAcquire = delegate { };
+        bool acquired = false;
+
+        IDisposable ILock.Acquire()
+        {
+            this.OnAcquire(this, this.counter);
+            this.acquired = true;
+            return this;
+        }
+
+        bool ILock.IsLocked
+        {
+            get
+            {
+                return this.acquired && !this.disposed;
+            }
+        }
+
+        public event EventHandler<T> OnDispose = delegate { };
+        bool disposed = false;
+        void IDisposable.Dispose()
+        {
+            this.OnDispose(this, this.counter);
+            this.disposed = true;
+        }
+
+        T counter;
+        internal void SetCounter(T counter)
+        {
+            this.counter = counter;
+        }
+    }
+}

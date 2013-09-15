57e21d36fb609642197ca411993deed07f30f321
Jay Bazuzi <jay@bazuzi.com>
2013-09-15 10:39:46 -0700

    RED: Add Class `MutexLockTests`

---
+++ LocksExperiment1/LocksExperiment1/MutexLockTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LocksExperiment1
+{
+    public class MutexLockTests : LockTests
+    {
+        protected override ILock Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

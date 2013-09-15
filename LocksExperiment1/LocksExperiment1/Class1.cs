64460049e491ae1c406ba6b9452c50264d9fab45
Jay Bazuzi <jay@bazuzi.com>
2013-09-15 09:16:46 -0700

    RED: Create project w/ failing test

---
+++ LocksExperiment1/LocksExperiment1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LocksExperiment1
+{
+    public class Class1
+    {
+    }
+
+    public class Tests
+    {
+        [Fact]
+        public void Fail()
+        {
+            Assert.True(false);
+        }
+    }
+}
